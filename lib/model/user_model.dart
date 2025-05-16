d7bf57749c86239330b691ee4e524ba85c34117a
ReachOut 003 <appuharikrishnan08@gmail.com>
2025-05-16 18:02:31 +0530

    brand

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -31,28 +31,30 @@ class UserModel {
     required this.sponsorAds,
   });
 
-  factory UserModel.fromJson(Map<String, dynamic> json) {
-    return UserModel(
-      id: json['id'],
-      email: json['email'],
-      createdAt: DateTime.parse(json['createdAt']),
-      lastActiveAt:
-          json['lastActiveAt'] != null ? DateTime.parse(json['lastActiveAt']) : null,
-      isVerified: json['isVerified'],
-      isPaymentStatus: json['isPaymentStatus'],
-      subscriptionExpiry: json['subscriptionExpiry'] != null
-          ? DateTime.parse(json['subscriptionExpiry'])
-          : null,
-      companyLogo: json['companyLogo'],
-      companyName: json['companyName'],
-      country: json['country'],
-      isAndroidUser: json['isAndroidUser'],
-      cranes: List<dynamic>.from(json['cranes']),
-      sponsorAds: (json['SponsorAds'] as List)
-          .map((adJson) => SponsorAdModel.fromJson(adJson))
-          .toList(),
-    );
-  }
+factory UserModel.fromJson(Map<String, dynamic> json) {
+  return UserModel(
+    id: json['id'],
+    email: json['email'],
+    createdAt: DateTime.parse(json['createdAt']),
+    lastActiveAt: json['lastActiveAt'] != null
+        ? DateTime.parse(json['lastActiveAt'])
+        : null,
+    isVerified: json['isVerified'],
+    isPaymentStatus: json['isPaymentStatus'],
+    subscriptionExpiry: json['subscriptionExpiry'] != null
+        ? DateTime.parse(json['subscriptionExpiry'])
+        : null,
+    companyLogo: json['companyLogo'],
+    companyName: json['companyName'],
+    country: json['country'],
+    isAndroidUser: json['isAndroidUser'],
+    cranes: json['cranes'] != null ? List<dynamic>.from(json['cranes']) : [],
+    sponsorAds: (json['SponsorAds'] as List<dynamic>? ?? [])
+        .map((adJson) => SponsorAdModel.fromJson(adJson))
+        .toList(),
+  );
+}
+
 
   Map<String, dynamic> toJson() {
     return {
